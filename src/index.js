f86a7a75466b79c9f5296943931aa583e8b46532
xlopec23 <xlopec23@tut.by>
2018-09-29 19:57:24 +0300

    alarm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  var e = str.length;
+
+  for (var c=0; c<e/2; c++) {
+    for (var i=0; i<bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i][0]+bracketsConfig[i][1], '');
+    }
+  }
+  return (str.length == 0);
 }
